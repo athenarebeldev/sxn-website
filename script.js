f7c0ed3759c7d63310c7c5893d8e780e9d059541
Stephen Carder <stephen@stafngrimr.com>
2023-05-03 16:28:45 +0100

    All the m friendly changes

---
+++ script.js
@@ -0,0 +1,29 @@
+function ready() {
+  if (screen.width <= 768) {
+    const nav = document.querySelector("nav");
+    
+    const link = document.createElement("a");
+    link.className = "icon";
+    link.href = "javascript:void(0)";
+    nav.appendChild(link);
+    link.onclick = function(){
+      const x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    };
+
+    const heading = document.createElement("h4");
+    const menu = document.createTextNode("Menu");
+
+    heading.appendChild(menu);
+    link.appendChild(heading);
+
+    const myLinks = document.getElementById("myLinks");
+    myLinks.style.display = "none";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", ready);
